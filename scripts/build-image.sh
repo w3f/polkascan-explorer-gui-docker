fc8c42c6cb73d8a7455705c3e398eca2cfb9fe32
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-26 16:28:14 +0200

    complete build image script with build args

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+git submodule update --init --recursive
+
+/scripts/build-image.sh \
+    web3f/polkascan-explorer-gui \
+    --build-arg API_URL \
+    --build-arg NETWORK_NAME \
+    --build-arg NETWORK_ID \
+    --build-arg NETWORK_TYPE \
+    --build-arg CHAIN_TYPE \
+    --build-arg NETWORK_TOKEN_SYMBOL \
+    --build-arg NETWORK_TOKEN_DECIMALS \
+    --build-arg NETWORK_COLOR_CODE \
+    -f polkascan-pre-explorer-gui/Dockerfile \
+    ./polkascan-pre-explorer-gui
